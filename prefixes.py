9c21cb18a737c55b6051598cdd6c5205d1b176e3
ST4RL1GHT <dryuakita@gmail.com>
2025-12-25 21:39:10 +0900

    Prefixes

---
+++ prefixes.py
@@ -0,0 +1,7 @@
+#prefixes
+line = 'I am learning Python programming language'
+line.startswith('I am')
+line.endswith('programming language')
+
+line.startswith('am learning')
+line.endswith('programming language')
